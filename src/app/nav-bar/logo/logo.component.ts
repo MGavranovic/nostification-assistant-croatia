0481323ef15332a5c68eb0e73b78a5a12eaebdb4
MGavranovic <mgavranovic7@gmail.com>
2024-03-23 14:29:37 +0100

    Updated readme

---
+++ src/app/nav-bar/logo/logo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'logo',
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.css',
+})
+export class LogoComponent {}
