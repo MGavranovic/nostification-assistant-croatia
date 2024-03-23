b3dd36b8b6391714bf0cb48960c5fa2b9e6f8862
MGavranovic <mgavranovic7@gmail.com>
2024-03-23 14:30:29 +0100

    Working on the Nav Bar and updated README

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchComponent } from './nav-bar/search/search.component';
+import { LogoComponent } from './nav-bar/logo/logo.component';
+import { MenuComponent } from './nav-bar/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SearchComponent,
+    LogoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
