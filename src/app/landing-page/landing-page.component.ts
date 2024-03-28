98d23e912d37a63b1616cd118a6c6caeb84ac6b0
MGavranovic <mgavranovic7@gmail.com>
2024-03-28 20:16:37 +0100

    test

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent {
+  landingPageStyleHide: string = '';
+  landingPageStyleShow: string = '';
 
+  onClickHideLanding(event: any): boolean {
+    console.log(event);
+    if (event.type === 'click') {
+      this.landingPageStyleHide =
+        'display: none, height: 82vh, justify-content: center';
+      return true;
+      // console.log('display: none, height: 82vh, justify-content: center');
+      // return 'display: none, height: 82vh, justify-content: center';
+    } else {
+      this.landingPageStyleShow =
+        'display: flex, height: 82vh, justify-content: center';
+      return false;
+      // console.log('display: flex, height: 82vh, justify-content: center');
+      // return 'display: flex, height: 82vh, justify-content: center';
+    }
+  }
 }
--- src/app/landing-page/main-classes/main-classes.component.ts
+++ src/app/landing-page/main-classes/main-classes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-main-classes',
