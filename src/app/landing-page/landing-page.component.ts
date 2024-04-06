f8b95841b58be1ded14493adb8d8681494eb07ee
MGavranovic <mgavranovic7@gmail.com>
2024-04-06 13:18:21 +0200

    Footer component text change implemented

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css',
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent {
   currentComponent: string = 'landingComponent';
 
+  currentComponentFooterText: string = 'Predmeti';
+
   toggleComponent(): void {
-    this.currentComponent =
-      this.currentComponent === 'landingComponent'
-        ? 'mainClassesComponent'
-        : 'landingComponent';
+    // this.currentComponent =
+    //   this.currentComponent === 'landingComponent'
+    //     ? 'mainClassesComponent'
+    //     : 'landingComponent';
+    if (this.currentComponent === 'landingComponent') {
+      this.currentComponent = 'mainClassesComponent';
+      this.currentComponentFooterText = 'O nama';
+      console.log(this.currentComponent, this.currentComponentFooterText);
+    } else {
+      this.currentComponent = 'landingComponent';
+      this.currentComponentFooterText = 'Predmeti';
+      console.log(this.currentComponent, this.currentComponentFooterText);
+    }
   }
 }
